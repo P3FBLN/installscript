d854b6c2f4f5042666d51569d9f4f864a5f35883
Philipp <46132640+P3FBLN@users.noreply.github.com>
2021-07-21 18:28:28 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+#ask for user to add
+while true
+do
+  echo "Please Enter new Username"
+  read -p 'Username: ' username
+  read -p "Frage: '$username' correct? " -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		break
+	fi
+done
